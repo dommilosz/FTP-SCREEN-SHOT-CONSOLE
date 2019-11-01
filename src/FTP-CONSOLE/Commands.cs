01aba9b82074d94589a0732c20bd18d431dc3874
milosz 123456 <dommilosz@gmail.com>
2019-11-01 11:11:42 +0100

    Upload

---
+++ src/FTP-CONSOLE/Commands.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WikipediaNet;
+
+namespace FTP_CONSOLE
+{
+    public class Commands
+    {
+        public static class WIKISEARCH
+        {
+            static int max = 5;
+            public static string Run(List<string> args)
+            {
+                string result = "";
+                if (LOGIN.loged)
+                {
+                    if (Program.GetArgs(args, 1).ToLower().Trim() == "#max")
+                    {
+                        SetMax(args);
+                        return "";
+                    }
+                    Program.WriteLn("::Results::", true, ConsoleColor.Yellow);
+                    Wikipedia w = new Wikipedia(WikipediaNet.Enums.Language.Polish);
+                    string q = Program.GetArgs(args, 1, -1);
+                    var rs = w.Search(q);
+                    int rstof = rs.Search.Count;
+                    if (rs.Search.Count > max) rstof = max;
+                    for (int i = 0; i < rstof; i++)
+                    {
+                        string raw = rs.Search[i].Snippet;
+                        string f = raw.Replace("<span class=\"searchmatch\">", "");
+                        f = f.Replace("</span>", "");
+                        Program.WriteLn(f, true, ConsoleColor.Magenta);
+                    }
+                    Program.WriteLn("::End:Results::", true, ConsoleColor.Yellow);
+                }
+                else { throw new Exception("Unknown Command"); }
+                return result;
+            }
+            public static string SetMax(List<string> args)
+            {
+                if (LOGIN.loged)
+                {
+                    max = Convert.ToInt32(Program.GetArgs(args, 2));
+                    Program.WriteLn($"Max Set to : {max}", true, ConsoleColor.Yellow);
+                }
+                return "";
+            }
+        }
+        public static class CLEAR
+        {
+            public static string Run(List<string> args)
+            {
+                Console.Clear();
+                return "";
+            }
+        }
+        public static class LOGIN
+        {
+            public static bool loged = false;
+            public static string Login(List<string> args)
+            {
+                Console.Clear();
+                if (Program.GetArgs(args, 1, -1).Trim() == "4628")
+                {
+                    loged = true;
+                }
+                if (Program.GetArgs(args, 1).Trim().ToLower() == "lo")
+                {
+                    loged = false;
+                }
+                if (loged)
+                    Program.WriteLn($"Logged : {loged}", true, ConsoleColor.Green);
+                else
+                    Program.WriteLn($"Logged : {loged}", true, ConsoleColor.Red);
+                return "";
+            }
+        }
+    }
+}
--- src/FTP-CONSOLE/Program.cs
+++ src/FTP-CONSOLE/Program.cs
@@ -8,8 +8,49 @@ namespace FTP_CONSOLE
 {
     class Program
     {
+        public static string GetArgs(List<string> args, int fromindex, int toindex = -2)
+        {
+            string rn = "";
+            List<string> arg = args;
+            if (toindex == -1) toindex = arg.Count - 1;
+            if (toindex == -2) toindex = fromindex;
+            for (int i = fromindex; i <= toindex; i++)
+            {
+                rn += arg[i] + " ";
+            }
+            return rn;
+        }
+        public static void WriteLn(string txt, bool line = true, ConsoleColor cl = ConsoleColor.White)
+        {
+            Console.ForegroundColor = cl;
+            if (line)
+            {
+                Console.WriteLine(txt);
+            }
+            else { Console.Write(txt); }
+            Console.ForegroundColor = ConsoleColor.White;
+        }
         static void Main(string[] args)
         {
+            while (true)
+            {
+                try
+                {
+                    List<string> argsl = Console.ReadLine().Split(' ').ToList();
+                    switch (GetArgs(argsl, 0).ToLower().Trim())
+                    {
+                        case "wikisearch":
+                        case "ws": Commands.WIKISEARCH.Run(argsl); break;
+                        case "clear":
+                        case "cs": Commands.CLEAR.Run(argsl); break;
+                        case "login":
+                        case "ln": Commands.LOGIN.Login(argsl); break;
+                        default:throw new Exception("Unknown Command");
+                    }
+
+                }
+                catch (Exception ex) { WriteLn(ex.Message, true, ConsoleColor.Red); }
+            }
         }
     }
 }
